62c6f2557da9b9450e8aedee8faf0bac02b7960a
Bolagam <133650186+Bolagam@users.noreply.github.com>
2023-05-24 20:42:54 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit
+
+streamlit.title('My Parents Nwe Healthy Diner')
+
+import streamlit 
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('kale ,Spinach & Rocket Smoothie')
+streamlit.text('Hard-Bolied Free-Range Egg')
